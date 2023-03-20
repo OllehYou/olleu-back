e1d552c6779c163af708962c73930df4979a9704
홍태호 <malamute10@xinapse.ai>
2023-03-20 13:43:37 +0900

    init commit

---
+++ src/main/java/com/example/olleuback/OlleuBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.olleuback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlleuBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OlleuBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/olleuback/OlleuBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.olleuback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlleuBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
