3060510389c4d1102a303e2a7264955d0996a282
홍태호 <malamute10@xinapse.ai>
2023-04-08 15:39:33 +0900

    resdocs 설정 및 Enum docs 생성 자동화

--- src/main/java/com/example/olleuback/common/olleu_enum/OlleUEnum.java
+++ src/main/java/com/example/olleuback/common/olleu_enum/OlleUEnum.java
@@ -1,17 +1,38 @@
 package com.example.olleuback.common.olleu_enum;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 public class OlleUEnum {
     @Getter
-    public enum FriendStatus {
-        INVITE,
-        FRIEND,
-        DELETE
+    @AllArgsConstructor
+    public enum FriendStatus implements EnumType{
+        INVITE("초대"),
+        FRIEND("친구"),
+        DELETE("삭제");
+        private final String description;
+        @Override
+        public String getName() {
+            return this.name();
+        }
+        @Override
+        public String getDescription() {
+            return this.description;
+        }
     }
     @Getter
-    public enum ParticipateStatus {
-        INVITE,
-        ACCEPT
+    @AllArgsConstructor
+    public enum ParticipateStatus implements EnumType{
+        INVITE("초대"),
+        ACCEPT("수락");
+        private final String description;
+        @Override
+        public String getName() {
+            return this.name();
+        }
+        @Override
+        public String getDescription() {
+            return this.description;
+        }
     }
 }
